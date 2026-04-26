00111bdee68a6878af98bf89ed31fbb19a85962d
Vaishnavi Pednekar <vaishnavi28pednekar@gmail.com>
2026-04-26 21:45:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DSA
+This Repositories include daily practice of DSA solving problem from beginner level to advanced.
---
+++ dayone.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class dayone {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // Patterns Making
+        
+        // 1. 
+        System.out.println("------PATTERN MAKING------");
+
+        System.out.println("enter the number: ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // 2.
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // 3.
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(i == 1 || j == 1 || i == n || j == n){
+                    System.out.print("*");
+                } else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // 4.
+        for(int i = n; i >0; i--){
+            for(int j = 0; j < i ; j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // 5.
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n - i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // 6.
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // 7.
+        for(int i = n; i >0; i--){
+            for(int j = 1; j <= i ; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
