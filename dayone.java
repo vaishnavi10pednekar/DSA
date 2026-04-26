d75e8528854f1f6d739d4503d06304e8f9439a87
Vaishnavi Pednekar <vaishnavi28pednekar@gmail.com>
2026-04-26 22:03:34 +0530

    Updated project

--- dayone.java
+++ dayone.java
@@ -79,5 +79,29 @@ public class dayone {
             System.out.println();
         }
         System.out.println();
+
+        // 8.
+        int number = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(" " + number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // 9.
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                if((i + j) % 2 == 0){
+                    System.out.print(1 + " ");
+                }else{
+                    System.out.print(0 + " ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
