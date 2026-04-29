c9327948f2c95dde4f7764c184ccc17efa89cb83
Vaishnavi Pednekar <vaishnavi28pednekar@gmail.com>
2026-04-29 21:33:50 +0530

    Updated project

--- daytwo.java
+++ daytwo.java
@@ -1,4 +1,8 @@
 import java.util.*;
 public class daytwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("hello world");
+    }
     
 }
